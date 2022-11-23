22e7d6ad890ae491ca481b63979821b11558658f
Kooreis <kooreis.official@gmail.com>
2022-11-22 21:00:00 -0600

    In this update, the user is prompted to enter an amount. The input is then read from the console, converted to a decimal, and stored in the 'amount' variable.

--- Currency_Converter.cs
+++ Currency_Converter.cs
@@ -1,17 +1,3 @@
-using System;
-using System.Collections.Generic;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Dictionary<string, decimal> currencyRates = new Dictionary<string, decimal>()
-        {
-            {"USD", 1.00m},
-            {"EUR", 0.85m},
-            {"GBP", 0.75m},
-            {"JPY", 110.15m},
-            {"AUD", 1.35m}
-        };
-    }
-}
+Console.WriteLine("Enter the amount:");
+string amountInput = Console.ReadLine();
+decimal amount = decimal.Parse(amountInput);
